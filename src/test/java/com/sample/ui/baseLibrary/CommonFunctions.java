2c3375f6b0dac2a5f8bb02734a47f5f94f5df6cc
mbhangariya <madhuri.bhangariya@gmail.com>
2021-11-26 21:29:47 +0530

    added beforesuite code and made changes in pom.xml

--- src/test/java/com/sample/ui/baseLibrary/CommonFunctions.java
+++ src/test/java/com/sample/ui/baseLibrary/CommonFunctions.java
@@ -1,5 +1,27 @@
 package com.sample.ui.baseLibrary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeSuite;
+
 public class CommonFunctions {
+	
+	public static String jsonFile;
+	static final Logger log=LoggerFactory.getLogger("CommonFunctions.class");
+	
+	@BeforeSuite
+	public void ReadCommandLineParamters()
+	{
+		String jsonFilePath=System.getProperty("jsonFilePath");
+		
+		if(jsonFilePath.equalsIgnoreCase("abc.json"))
+		{
+			jsonFile="src//test//resources//jsonFiles//abc.json";
+			System.out.println("file path is: " +jsonFile);
+			log.info("file path is: " +jsonFile);
+		}
+		
+		
+	}
 
 }
