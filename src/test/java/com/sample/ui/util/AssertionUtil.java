4842c0de5211972b058689a0e579123470e490d7
madhuribhangariya <madhuri.bhangariya@gmail.com>
2022-01-31 18:12:24 +0530

    :added some more code

--- src/test/java/com/sample/ui/util/AssertionUtil.java
+++ src/test/java/com/sample/ui/util/AssertionUtil.java
@@ -1,5 +1,23 @@
 package com.sample.ui.util;
 
+import org.openqa.selenium.WebElement;
+
 public class AssertionUtil {
+
+	public static void assertVisible(WebElement element) {
+
+	}
+
+	public static void assertNotVisible(WebElement element) {
+
+	}
+
+	public static void assertTextPresent(WebElement element) {
+
+	}
+
+	public static void addVerificationLogger(String message, String status) {
+
+	}
 
 }
--- src/test/java/com/sample/ui/util/JsonUtil.java
+++ src/test/java/com/sample/ui/util/JsonUtil.java
@@ -1,5 +1,27 @@
 package com.sample.ui.util;
 
+import org.json.JSONObject;
+
+import com.google.gson.JsonElement;
+
 public class JsonUtil {
+	
+	public static JSONObject readJsonFromFile(String file)
+	{
+		return null;
+		
+	}
+	
+	public static Object read(JsonElement json)
+	{
+		return json;
+		
+	}
+	
+	public static String getData(JSONObject json,String key)
+	{
+		return key;
+		
+	}
 
 }
